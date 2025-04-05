f6871a97739d9ab70a2348588758687834e0083b
lucasbrentano <lucasbrentano@gmail.com>
2025-04-05 15:29:05 -0300

    Lesson 4 started

--- app.js
+++ app.js
@@ -1,59 +1,38 @@
-import http from 'http'
-import fs from 'fs'
-import routes from './routes.js'
 import sqlite3 from 'sqlite3'
+import express from 'express'
+
 import { sequelize } from './models.js'
 
-const db = new sqlite3.Database('./tic.db', (err) => {
-    if(err) {
-        console.log('Error connecting to the database');
-        return;
-    }
-    console.log('Connected to the database');
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('Automatic message');
+    next();
 });
 
-fs.writeFile('./message.txt', "Hello, from the file!", 'utf8', (error) => {
-    if(error) {
-        console.log("Error writing to the file!", error);
-        return;
-    }
-    console.log("File successfully created!");
+app.use((req,res,next)=>{
+    console.log('Human message');
+    res.send({ msg:'hello world' });
 });
 
-fs.readFile('./message.txt', 'utf8', (error, content) => {
-    if(error) {
-        console.log("Error reading the file!")
-        return;
-    }
-
-    console.log(`Content: ${content}`);
-
-    startHttpServer(content);
+app.use((req,res,next)=>{
+    console.log('Download message');
 });
 
-async function startHttpServer(content) {
+async function startApp() {
+    const db = new sqlite3.Database('./tic.db', (err) => {
+        if(err) {
+            console.log('Error connecting to the database');
+            return;
+        }
+        console.log('Connected to the database');
+    });
+
     await sequelize.sync();
 
-    const server = http.createServer((req, res) => {
-        routes(req, res, {content});
-    });
-
     const port = 3000;
-    const host = 'localhost';
 
-    server.listen(port, host, () => {
-        console.log(`Server start in http://${host}:${port}/`);
-    });
+    app.listen(port)
 }
 
-function traditional() {
-    console.log('Traditional');
-};
-
-const expression = function() {
-    console.log('Expression');
-};
-
-const arrow = () => {
-    console.log('Arrow');
-};
+startApp();
--- models.js
+++ models.js
@@ -85,3 +85,98 @@ export async function deleteProduct(id) {
         throw error;
     }
 }
+
+const Order = sequelize.define('order', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    amount: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const ProductsOrder = sequelize.define('products_order', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    productId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }
+    },
+    orderId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Order,
+            key: 'id'
+        }
+    }
+});
+
+
+Product.belongsToMany(Order, { through: ProductsOrder });
+Order.belongsToMany(Product, { through: ProductsOrder });
+
+export async function createOrder(newOrder) {
+    try {
+        const order = await Order.create({
+            amount: newOrder.amount,
+            status: 'FORWARDED'
+        });
+
+        for (const prod of newOrder.products) {
+            const product = await Product.findByPk(prod.id);
+            if (product) {
+                order.addProduct(product, { through: { quantity: prod.quantity, price: product.price } });
+            }
+        }
+
+        console.log('Order successfully created');
+
+        return order;
+    } catch (error) {
+        console.log('Error creating Order', error);
+        throw error;
+    }
+}
+
+export async function getAllOrders() {
+    try {
+        const result = await ProductsOrder.findAll();
+        console.log('Orders found successfully', result);
+        return result;
+    } catch (error) {
+        console.log('Error getting all orders', error);
+        throw error;
+    }
+}
+
+export async function getOrderById(id) {
+    try {
+        const result = await Order.findByPk(id);
+        console.log('Order found successfully', result);
+        return result;
+    } catch (error) {
+        console.log('Error getting order', error);
+        throw error;
+    }
+}
